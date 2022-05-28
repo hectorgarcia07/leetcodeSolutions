7776c85ae96c5c92c4ded85fda4601137dbc017f
hectorgarcia07 <csharp12321@gmail.com>
2022-05-27 21:21:55 -0500

    improved solution

--- 347_Top_K_Frequent_Elements.js
+++ 347_Top_K_Frequent_Elements.js
@@ -4,7 +4,7 @@ https://leetcode.com/problems/top-k-frequent-elements/
 Runtime: O(n+mlogm) where n is the nums and mlogm is the sorting time to sort arr
 Space: O(n) 
 */
-var topKFrequent = function(nums, k) {
+/* var topKFrequent = function(nums, k) {
   let map = new Map()
   let answer = []
 
@@ -24,6 +24,40 @@ var topKFrequent = function(nums, k) {
   }
   console.log(map)
   return answer
+} */
+
+//New solution is O(n)
+var topKFrequent = function(nums, k) {
+  let map = new Map()
+  let arr = Array.from(Array(nums.length + 1), () => [])
+
+  //map all the frequency of numbers
+  for(let num of nums){
+    if(map.has(num)){
+      map.set(num, map.get(num) + 1)
+    }else{
+      map.set(num, 1)
+    }
+  }
+
+  //bucket sort the number of frequencies
+  map.forEach((val, key) => {
+    arr[val].push(key)
+  })
+  
+  let answer = []
+  //find the k number of frequencies
+  for(let i = arr.length - 1; i >= 0; i--){
+    if(arr[i].length != 0){
+      for(let num of arr[i]){
+        answer.push(num)
+        if(answer.length == k){
+          return answer
+        }
+      }
+    }
+  }
+  return answer
 }
 
-console.log(topKFrequent([3,0,1,0], 1))
+console.log(topKFrequent([1], 1))
